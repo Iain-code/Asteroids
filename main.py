88030ca4768ff447456c3652f654e07c0c497f04
iain-code <iainvirgo1010@live.co.uk>
2024-08-21 20:49:57 +0300

    Triangle added to black screen

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
-clock = pygame.time.Clock()
-dt = 0
+from player import Player
+
+
 def main():
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)  
         pygame.display.flip()
         dt = clock.tick(60) / 1000
       
