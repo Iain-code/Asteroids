36e1271d58ba64b31961bc7c5e9c302abbbb3f45
iain-code <iainvirgo1010@live.co.uk>
2024-08-23 20:09:17 +0300

    Rotation of triangle

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)  
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
       
 
 
